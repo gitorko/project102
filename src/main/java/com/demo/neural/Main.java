b9b3d4f823fcb0db84a7e2c7a2091cf75ac75f26
Arjun Surendra <asurendra@vmware.com>
2024-02-23 16:53:43 +0530

    code

--- src/main/java/com/demo/neural/Main.java
+++ src/main/java/com/demo/neural/Main.java
@@ -56,7 +56,7 @@ public class Main {
     }
 
     @Bean
-    public CommandLineRunner sendData() {
+    public CommandLineRunner start() {
         return args -> {
             generateModel();
             log.info("Flower type is {}", predictForInput(new float[]{5.1f, 3.5f, 1.4f, 0.2f}));
